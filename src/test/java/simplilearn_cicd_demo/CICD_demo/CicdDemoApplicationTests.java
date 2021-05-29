4a15f8c857bdb9123d82603b68a580516a2595e8
student <sandeepkumar41993@yahoo.in>
2021-05-29 14:53:09 +0000

    spring project build

---
+++ src/test/java/simplilearn_cicd_demo/CICD_demo/CicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package simplilearn_cicd_demo.CICD_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
